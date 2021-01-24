51b3a836e35871e2362b0f81603f6ac162e11eb8
Cynthia Salazar <cinthya3259@gmail.com>
2021-01-23 22:14:02 -0600

    Versión 2

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('a').click(function(event) {
+        if (this.hash !=="") {
+            event.preventDefault()
+            var hash = this.hash
+            $('html, body').animate({
+                scrollTop: $(hash).offset().scrollTop
+            },800,function() {
+                window.location.hash = hash
+            })
+        }
+    })
+})
+
+
+$(function() {
+    $('{data-toggle="tooltip"}').tooltip()
+})
+
